eeb53b6f2c92688752b3e9554f544a7898bc5d47
Curtis Weeks <curtis@jabbtech.com>
2019-01-27 20:27:08 -0600

    staging some content to migrate

--- public/js/lrtf.js
+++ public/js/lrtf.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
 
-  // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
+	// Check for click events on the navbar burger icon
+	$(".navbar-burger").click(function() {
 
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+		// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+		$(".navbar-burger").toggleClass("is-active");
+		$(".navbar-menu").toggleClass("is-active");
 
-  });
+	});
+
+	var mapOptions = {
+		zoom: 16,
+		center: new google.maps.LatLng(34.7480943, -92.2725005),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var map = new google.maps.Map(document.getElementById("is-map"), mapOptions);
+
 });
