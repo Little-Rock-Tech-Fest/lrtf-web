ccb21f2dfd18d33f95e1985d0a8a270b2ee14a28
Curtis Weeks <curtis@jabbtech.com>
2019-01-30 20:01:01 -0600

    start to migrate sponsor content

--- routes/middleware.js
+++ routes/middleware.js
@@ -20,8 +20,9 @@ var _ = require('lodash');
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		//{ label: 'Home', key: 'home', href: '/' },
-		{ label: 'Blog', key: 'blog', href: '/blog' },
-		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
+		//{ label: 'Blog', key: 'blog', href: '/blog' },
+		//{ label: 'Gallery', key: 'gallery', href: '/gallery' },
+		{ label: 'Sponsor', key: 'sponsor', href: '/sponsor' },
 		{ label: 'Contact', key: 'contact', href: '/contact' },
 	];
 	res.locals.user = req.user;
