7e82710d2f57f1e84ffcf69882179d20da2b7ff0
Curtis Weeks <curtis@jabbtech.com>
2019-03-19 21:15:54 -0500

    passes sponsor data to the view

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,13 +1,41 @@
 var keystone = require('keystone');
+//var async = require('async');
 
 exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
 	locals.section = 'home';
+	locals.data = {
+		sponsors: [],
+		donations: [],
+	}
+
+	view.on('init', function (next) {
+		
+		var q = keystone.list('Donation').model.find()
+			.where('year', '2019');
+
+		q.exec(function (err, results) {
+			locals.data.donations = results;
+			next(err);
+		});
+
+	});
+
+	view.on('init', function (next) {
+
+		var q = keystone.list('Sponsor').model.find()
+			.populate('donations');
+
+		q.exec(function (err, results) {
+			//console.log(results);
+			locals.data.sponsors =  results;
+			next(err);
+		});
+
+	});
 
 	// Render the view
 	view.render('index');
