00a2c1e4d86addae7bb4c700a067769631b700e6
Curtis Weeks <curtis@laffertyequipment.com>
2019-08-23 18:39:19 -0500

    feature four randomly selected speakers on page load

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,23 +1,33 @@
 var keystone = require('keystone');
 //var async = require('async');
 
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
 exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
+	var request = require('request');
 
 	locals.section = 'home';
 	locals.data = {
 		sponsors: [],
+		speakers: [],
 		donations: [],
 	}
 
 	view.on('init', function (next) {
-		
+
 		var q = keystone.list('Donation').model.find()
-			.populate('sponsors')
-			.sort('weight')
-			.where('year', '2019');
+		.populate('sponsors')
+		.sort('weight')
+		.where('year', '2019');
 
 		q.exec(function (err, results) {
 			locals.data.donations = results;
@@ -29,11 +39,34 @@ exports = module.exports = function (req, res) {
 	view.on('init', function (next) {
 
 		var q = keystone.list('Sponsor').model.find()
-			.populate('donations');
+		.populate('donations');
 
 		q.exec(function (err, results) {
 			locals.data.sponsors =  results;
 			next(err);
+		});
+
+	});
+
+	view.on('init', function (next) {
+
+		request('https://sessionize.com/api/v2/vmstvnf4/view/speakers', function (error, response, body) {
+
+			if (error) {
+				return console.dir(error)
+			}
+
+			var speakers = [];
+			var json = JSON.parse(body);
+			for (speaker in json) {
+				speakers.push(json[speaker]);
+			}
+			shuffle(speakers);
+			speakers = speakers.slice(0, 4);
+
+			locals.data.speakers = speakers;
+			next();
+
 		});
 
 	});
