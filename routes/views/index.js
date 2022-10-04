e101ee8a9da82571618d6e845bccc5871495ffea
Daniel Pollock <dpollock@cteh.com>
2022-10-04 00:11:43 -0500

    show sponsors

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,71 +1,72 @@
-var keystone = require('keystone');
+var keystone = require("keystone");
 //var async = require('async');
 
 function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
+	for (let i = a.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[a[i], a[j]] = [a[j], a[i]];
+	}
+	return a;
 }
 
 exports = module.exports = function (req, res) {
-
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-	var request = require('request');
+	var request = require("request");
 
-	locals.section = 'home';
+	locals.section = "home";
 	locals.data = {
 		donations: [],
 		sponsors: [],
 		speakers: [],
 		members: [],
-	}
+	};
 
-	view.on('init', function (next) {
+	view.on("init", function (next) {
+		locals.data.sponsors = require("../../sponsors.json");
+		// var Donation = keystone.list('Donation');
+		// var Sponsor = keystone.list('Sponsor');
+		// var Member = keystone.list('Member');
 
-		var Donation = keystone.list('Donation');
-		var Sponsor = keystone.list('Sponsor');
-		var Member = keystone.list('Member');
+		// Donation.model.find()
+		// 	.populate('sponsors')
+		// 	.sort('weight')
+		// 	.where('year',  '2022')
+		// 	.exec(function (err, donations) {
+		// 		locals.data.donations = donations;
+		// 	});
 
-		Donation.model.find()
-			.populate('sponsors')
-			.sort('weight')
-			.where('year',  '2022')
-			.exec(function (err, donations) {
-				locals.data.donations = donations;
-			});
+		// Sponsor.model.find()
+		// 	.populate('donations')
+		// 	.exec(function (err, sponsors) {
+		// 		locals.data.sponsors = sponsors;
+		// 	});
 
-		Sponsor.model.find()
-			.populate('donations')
-			.exec(function (err, sponsors) {
-				locals.data.sponsors = sponsors;
-			});
+		// Member.model.find()
+		// 	.exec(function (err, members) {
+		// 		locals.data.members = members;
+		// 	});
 
-		Member.model.find()
-			.exec(function (err, members) {
-				locals.data.members = members;
-			});
-
-		request('https://sessionize.com/api/v2/picvw5z5/view/Speakers', function (error, response, body) {
-			if (error) { 
-				return console.dir(error); 
-			}
-			var speakers = [];
-			var json = JSON.parse(body);
-			for (var speaker in json) {
-				//if (json[speaker].isTopSpeaker === false) {
+		request(
+			"https://sessionize.com/api/v2/picvw5z5/view/Speakers",
+			function (error, response, body) {
+				if (error) {
+					return console.dir(error);
+				}
+				var speakers = [];
+				var json = JSON.parse(body);
+				for (var speaker in json) {
+					//if (json[speaker].isTopSpeaker === false) {
 					speakers.push(json[speaker]);
-				//}
+					//}
+				}
+				shuffle(speakers);
+				speakers = speakers.slice(0, 4);
+				locals.data.speakers = speakers;
+				next();
 			}
-			shuffle(speakers);
-			speakers = speakers.slice(0,4);
-			locals.data.speakers = speakers;
-			next();
-		});		
-
+		);
 	});
 
-	view.render('index');
+	view.render("index");
 };
